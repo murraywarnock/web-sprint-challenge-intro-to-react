95b7f86df4994aaeb8b9e7aca7da6b1fd92d8b5b
MurrayWarnock <murray.warnock@gmail.com>
2021-04-16 10:09:40 -0600

    Receiving API data in App.js, displaying in Character.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import { BASE_URL } from './constants'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios.get(`${BASE_URL}people`)
+      .then(res => {
+        console.log(res);
+         setCharacters(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      console.log("Characters: ", characters)
+    }, []);
+     
   return (
     <div className="App">
-      <h1 className="Header">CharactersTEST</h1>
+      <h1 className="Header">Characters</h1>
+      {
+        characters.map(char => {
+          return <Character 
+                    key={char.index} 
+                    char={char}                       
+                  />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React from 'react'
+// import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
+
+// const { key, char } = props;
+
+const StyledLine = styled.div`
+  display: flex;
+  border: solid black 1px;
+//   margin: 10%
+//   padding: 10%
+  `
+
+export default function Character({ key, char }) {
+    // const 
+    return (
+        <div>
+        <StyledLine>
+            <p>
+            {char.name} 
+            </p>       
+            {/* <button {onClick(() => {})}>Detail</button> */}
+            <p>
+            <button>DETAIL</button>
+            </p>
+        </StyledLine>
+        </div>
+    )
+};
+
+// export default Character;
